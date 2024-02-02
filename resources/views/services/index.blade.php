2ac2d9033aa6d72376ea21e636c7f28ad75e2ab9
zouheirrhoumri <zouheir.rhoumri@gmail.com>
2024-02-02 14:53:18 +0100

    fix

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -33,7 +33,7 @@
                                 <a href="javascript:void(0)" class="flex items-center bg-yellow-200 rounded-xl font-bold text-sm text-yellow-900 py-3 px-4">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" class="text-lg mr-4" viewBox="0 0 16 16">
                                         <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
-                                    </svg>Plan
+                                    </svg>home
                                 </a>
                             </li>
                             <li>
@@ -54,7 +54,7 @@
                                 <a href="{{route('service.create')}} " class="inline-flex items-center justify-center h-9 px-5 rounded-xl bg-gray-900 text-gray-300 hover:text-white text-sm font-semibold transition">
 
                                     add
-                                    >
+                                    
                                 </a>
 
                             </div>
